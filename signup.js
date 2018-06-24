08b39e44e1072010aeb920228d20773717175fe1
KuluBig <kembertha348@gmail.com>
2018-06-24 14:20:48 +0300

    final of challenge 1

--- signup.js
+++
@@ -1,7 +0,0 @@
-function signup() {
-    var username = document.forms["signup"]["username"].value;
-    console.log(username);
-    document.getElementByName("result").innerHTML = username;
-    //alert(username);
-    //alert("testing")
-}
